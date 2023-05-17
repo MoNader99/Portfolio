d2805ae17b48e74213c40538a0b16fc8fce92733
MohammedNader <mnader93@yahoo.com>
2023-05-17 21:04:44 +0300

    editing the stack and adding animations

--- src/components/HomeComponents/AboutSection/About.js
+++ src/components/HomeComponents/AboutSection/About.js
@@ -8,27 +8,79 @@ const AboutSection = () => {
             <div class="hero-main row">
                 <div class="hero-text col-md-7 ">
                     <h1>Software Developer</h1>
-                    <p>Hi, I'm Mohammed Nader. A passionate Software Developer based in New Cairo, Egypt. 📍</p>
-                    <span>
+                    <p className='pp'>Hi, I'm Mohammed Nader. A passionate Software Developer based in New Cairo, Egypt. 📍</p>
+                    <div className='social'>
                         <a style={{fontSize: "3rem" , marginRight:"1.2rem", color:"#457B9D"}} aria-label="linkedin" rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/monader99/">
                             <i class="fa-brands fa-linkedin"></i>
                         </a>
                         <a style={{fontSize: "3rem" , color:"#457B9D"}} aria-label="github" rel="noreferrer" target="_blank" href="https://github.com/MoNader99">
                             <i class="fa-brands fa-github"></i>
-                        </a></span>
+                        </a></div>
                     </div>
                 <div class="hero-img col-md-5 d-flex justify-content-center align-items-center"></div>
             </div>
         </div>
 
-        <p className='my-4'>Tech Stack</p>
+        <p style={{marginLeft:"100px"}} className='tech-stack my-4'>Tech Stack</p>
         <div class="skills">
             <div class="logos ">
             <ul>
                 <li>
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" title="React" alt="React"/>
+                </li>
+                <li>
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg" title="Redux" alt="Redux"/>
+                </li>
+                <li>
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/dot-net/dot-net-original-wordmark.svg" title=".NET" alt=".NET"/>
+                </li>
+                <li>
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" title="Node.js" alt="Node.js"/>
+                </li>
+                <li>
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" title="MongoDB" alt="MongoDB"/>
+                </li>
+                <li>
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" title="MySQL" alt="MySQL"/>
+                </li>
+                <li>
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" title="PostgreSQL" alt="PostgreSQL"/>
+                </li>
+                <li>
                     <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" title="TypeScript" alt="TypeScript"/>
                 </li>
                 <li>
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" title="JavaScript" alt="JavaScript"/>
+                </li>
+                <li>
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" title="Sass" alt="Sass"/>
+                </li>
+                <li>
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" title="Bootstrap" alt="Bootstrap"/>
+                </li>
+                <li>
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" title="HTML5" alt="HTML5"/>
+                </li>
+                <li>
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" title="CSS3" alt="CSS3"/>
+                </li>
+                <li>
+                    <img src="https://www.vectorlogo.zone/logos/flutterio/flutterio-icon.svg" title="Flutter" alt="Flutter"/>
+                </li>
+                <li>
+                    <img src="https://www.vectorlogo.zone/logos/dartlang/dartlang-icon.svg" title="Dart" alt="Dart"/>
+                </li>
+                <li>
+                    <img src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" title="Figma" alt="Figma"/>
+                </li>
+                <li>
+                    <img src="https://www.vectorlogo.zone/logos/pocoo_flask/pocoo_flask-icon.svg" title="Flask" alt="Flask"/>
+                </li>
+                
+                <li>
+                    <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" title="Git" alt="Git"/>
+                </li>
+                {/* <li>
                     <img src="https://www.vectorlogo.zone/logos/tensorflow/tensorflow-icon.svg" title="TensorFlow" alt="TensorFlow"/>
                 </li>
                 <li>
@@ -38,74 +90,22 @@ const AboutSection = () => {
                     <img src="https://upload.wikimedia.org/wikipedia/commons/0/05/Scikit_learn_logo_small.svg" title="scikit-learn" alt="scikit-learn"/>
                 </li>
                 <li>
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" title="Sass" alt="Sass"/>
-                </li>
-                <li>
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" title="Bootstrap" alt="Bootstrap"/>
-                </li>
-                <li>
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg" title="Redux" alt="Redux"/>
-                </li>
-                <li>
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" title="React" alt="React"/>
-                </li>
-                <li>
                     <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" title="C++" alt="C++"/>
-                </li>
-                <li>
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" title="CSS3" alt="CSS3"/>
-                </li>
-                <li>
-                    <img src="https://www.vectorlogo.zone/logos/dartlang/dartlang-icon.svg" title="Dart" alt="Dart"/>
-                </li>
+                </li>                
                 <li>
                     <img src="https://cdn.worldvectorlogo.com/logos/django.svg" title="Django" alt="Django"/>
                 </li>
-                <li>
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/dot-net/dot-net-original-wordmark.svg" title=".NET" alt=".NET"/>
-                </li>
-                <li>
-                    <img src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" title="Figma" alt="Figma"/>
-                </li>
-                <li>
-                    <img src="https://www.vectorlogo.zone/logos/pocoo_flask/pocoo_flask-icon.svg" title="Flask" alt="Flask"/>
-                </li>
-                <li>
-                    <img src="https://www.vectorlogo.zone/logos/flutterio/flutterio-icon.svg" title="Flutter" alt="Flutter"/>
-                </li>
-                <li>
-                    <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" title="Git" alt="Git"/>
-                </li>
-                <li>
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" title="HTML5" alt="HTML5"/>
-                </li>
-                <li>
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" title="JavaScript" alt="JavaScript"/>
-                </li>
-                <li>
-                    <img src="https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg" title="Jest" alt="Jest"/>
-                </li>
+                
                 <li>
                     <img src="https://upload.wikimedia.org/wikipedia/commons/2/21/Matlab_Logo.png" title="MATLAB" alt="MATLAB"/>
-                </li>
-                <li>
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" title="MongoDB" alt="MongoDB"/>
-                </li>
-                <li>
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" title="MySQL" alt="MySQL"/>
-                </li>
-                <li>
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" title="Node.js" alt="Node.js"/>
-                </li>
+                </li>                
                 <li>
                     <img src="https://www.vectorlogo.zone/logos/opencv/opencv-icon.svg" title="OpenCV" alt="OpenCV"/>
                 </li>
                 <li>
                     <img src="https://raw.githubusercontent.com/devicons/devicon/2ae2a900d2f041da66e950e4d48052658d850630/icons/pandas/pandas-original.svg" title="pandas" alt="pandas"/>
                 </li>
-                <li>
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" title="PostgreSQL" alt="PostgreSQL"/>
-                </li>
+                
                 <li>
                     <img src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" title="Postman" alt="Postman"/>
                 </li>
@@ -117,7 +117,7 @@ const AboutSection = () => {
                 </li>
                 <li>
                     <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Qt_logo_2016.svg" title="Qt" alt="Qt"/>
-                </li>
+                </li> */}
             </ul>
 
             </div>
--- src/components/HomeComponents/BriefSection/Brief.js
+++ src/components/HomeComponents/BriefSection/Brief.js
@@ -7,13 +7,13 @@ const BriefSection = () => {
         <div className='content'>
             <div className='row'>
                 <div className='col-lg-5 col-sm-12'>
-                    <img src='https://www.espine.in/blog/wp-content/uploads/2022/08/software-developer.jpg' alt='nothing'/>
+                    <img src='https://img.freepik.com/premium-vector/laptop-coding-concept-web-developer-programming-laptop-screen-code-illustration_100456-1753.jpg' alt='nothing'/>
                 </div>
                 <div className='col-lg-7 col-sm-12'>
                     <div class="text-side">
                         <h4 style={{fontWeight: '700', color: '#023e8a'}}>About me</h4>
                         <h3 style={{fontSize: '1.2rem',fontWeight: '900'}}>A dedicated Software Developer based in New Cairo, Egypt 📍</h3>
-                        <p style={{color:"#767676"}}>As a Junior Software Developer, I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, Tailwind, and SCSS. utilizing them to create visually appealing and responsive user interfaces. Additionally, I have a strong background in machine learning, computer vision, and natural language processing (NLP). I have successfully implemented models using TensorFlow and Keras, and I am adept at leveraging visualization libraries such as Seaborn and Matplotlib to analyze and present data effectively.</p>
+                        <p style={{color:"rgb(48 48 48)"}}>As a Junior Software Developer, I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, Tailwind, and SCSS. utilizing them to create visually appealing and responsive user interfaces, I offer designing and maintaining responsive websites that offer a smooth user experience. My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimized code and utilizing cutting-edge development tools and techniques. I am also a team player who thrives in collaborating with cross-functional teams to produce outstanding web applications.</p>
                     </div>
                 </div>
             </div>
